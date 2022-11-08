97b77d5960a257ddba97e57a356695beec4b8b74
a <rudolf.aion@gmail.com>
2022-11-08 12:56:53 +0000

    create ChildPropsB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import  { ChildPropsA }  from "./components/ChildPropsA"
+import  { ChildPropsB }  from "./components/ChildPropsB"
 import React, { useState } from "react"
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <ChildPropsA fromUser={fromUser} setFromUser={setFromUser}/>
+      <ChildPropsB fromUser={fromUser} setFromUser={setFromUser}/>
       {fromUser}
     </div>
   );
--- src/components/ChildPropsB.jsx
+++ src/components/ChildPropsB.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 
-export const ChildPropsA = (props) => {
+export const ChildPropsB = (props) => {
     return(
         <div>
             {props.fromUser}
-            <button onClick={() => props.setFromUser('bbb')}>change</button>
+            <button onClick={() => props.setFromUser('ccc')}>change</button>
         </div>
     )
 }
